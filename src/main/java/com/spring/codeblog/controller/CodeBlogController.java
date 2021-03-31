f5c20ac06663dc9865abc83e716ec5f07219239b
Ananias Raphael <ananias.nobrega@gmail.com>
2021-03-31 11:40:56 -0300

    model and view implementation

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -1,4 +1,26 @@
 package com.spring.codeblog.controller;
 
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
 public class CodeBlogController {
+
+    @Autowired
+    CodeblogService codeblogService;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
 }
