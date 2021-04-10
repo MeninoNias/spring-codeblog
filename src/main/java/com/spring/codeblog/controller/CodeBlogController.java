1ed3dcc97f3ebabbcd4dbd4b5c63756894e2e999
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-10 20:27:37 -0300

    post details

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class CodeBlogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
