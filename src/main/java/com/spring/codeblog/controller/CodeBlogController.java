22705ca0341e973489a3e24a37183e63d1b53d8f
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-11 14:20:48 -0300

    templates and controller modificações

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -4,11 +4,15 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,5 +35,21 @@ public class CodeBlogController {
         Post post = codeblogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+
     }
 }
